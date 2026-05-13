bfa33d680a2b068d51e4a9aea4deea4c90b46871
Rinthiya15 <rinthiya1508@gmail.com>
2026-05-13 10:31:23 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,7 +2,7 @@ import cocotb
 from cocotb.triggers import RisingEdge, Timer
 
 
-async def reset(dut):
+async def reset_dut(dut):
     dut.rst_n.value = 0
     dut.ui_in.value = 0
     dut.uio_in.value = 0
@@ -15,24 +15,38 @@ async def reset(dut):
 @cocotb.test()
 async def sr_flipflop_test(dut):
 
-    await reset(dut)
+    # Reset
+    await reset_dut(dut)
 
+    # ----------------
     # HOLD (S=0 R=0)
+    # ----------------
     dut.ui_in.value = 0b00
     await RisingEdge(dut.clk)
 
+    # ----------------
     # SET (S=1 R=0)
+    # ----------------
     dut.ui_in.value = 0b01
     await RisingEdge(dut.clk)
 
+    # ----------------
     # HOLD
+    # ----------------
     dut.ui_in.value = 0b00
     await RisingEdge(dut.clk)
 
+    # ----------------
     # RESET (S=0 R=1)
+    # ----------------
     dut.ui_in.value = 0b10
     await RisingEdge(dut.clk)
 
+    # ----------------
     # INVALID (S=1 R=1)
+    # ----------------
     dut.ui_in.value = 0b11
     await RisingEdge(dut.clk)
+
+    # small delay to allow simulator finish cleanly
+    await Timer(10, units="ns")
